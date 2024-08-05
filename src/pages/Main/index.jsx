b3fef51eb22685f30af889daad92cd4e06f5f029
Floki-gig <alexeyorekhov2005@mail.ru>
2024-08-05 16:54:51 +0500

    add navbar

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,10 +1,12 @@
 import {Services} from '../../components';
+import Navbar from '../../components/navbar/Navbar';
 import s from './style.module.scss';
 
 const MainPage = () => {
   return (
     <div>
       MainPage
+      <Navbar />
       <Services />
     </div>
   );
